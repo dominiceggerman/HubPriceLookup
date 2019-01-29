dea4fd3d54ab2e93388f551610e0403721f9778c
dominiceggerman <dominiceggerman@gmail.com>
2019-01-29 10:13:25 -0600

    Accessed pricing data

---
+++ hubLook.py
@@ -0,0 +1,39 @@
+# Imports
+import pandas as pd
+import psycopg2
+import readfile
+
+
+# Access DB
+def accessDB(user, password):
+    # Connect
+    conn = psycopg2.connect(dbname="gasaprod", user=user, password=password, host="gasaproddb02.genscape.com", port="5432")
+    cursor = conn.cursor()
+
+    # SQL
+    sql = """SELECT nd.issue_date, npp.price_point_name, npp.ngi_region_name, nd.average 
+            FROM ts1.ngi_daily AS nd
+            INNER JOIN ts1.ngi_price_point AS npp ON npp.pointcode = nd.pointcode
+            WHERE nd.trade_date BETWEEN current_date - INTERVAL '120 days' AND current_date
+        """
+
+    # Execute SQL
+    try:
+        cursor.execute(sql)
+        results = cursor.fetchall()
+    except:
+        print("Error while executing SQL...")
+        return None
+
+    # Close and return
+    cursor.close()
+    return pd.DataFrame(results, columns=["Issue Date", "Price Point Name", "Region Name", "Average Price"])
+
+
+# Run
+if __name__ == "__main__":
+    # Get creds
+    user, password = readfile.readFile("creds.txt")
+    # Get data
+    prices = accessDB(user, password)
+    

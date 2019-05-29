ff74fa15450a31684326614510a96a9b23915a39
dominiceggerman <dominiceggerman@gmail.com>
2019-05-29 10:01:27 -0500

    reworking email etc.

--- hubLook.py
+++ hubLook.py
@@ -43,43 +43,6 @@ if __name__ == "__main__":
     
     # Statistics
     hub_prices = prices.pivot_table(values="Average Price", index=["Issue Date"], columns=["Price Point Name"])
+    print(hub_prices.head())
 
-    hub_stats = {}
-    for hub in hub_prices.columns:
-        data = hub_prices[hub][hub_prices[hub] != 0]
-        
-        # Calc day 120 avg and 7 day average
-        avg = round(sum(data) / len(data), 2)
-        if 0 not in hub_prices[hub][-10:]:
-            wk_avg = round(sum(data[-7:]) / 7, 2)
-        else:
-            wk_avg = None
-        
-        # Calc DoD difference
-        today = round(hub_prices[hub].iloc[-1], 2)
-        dod = round(today - hub_prices[hub].iloc[-2], 2)
-
-        # Add to stats dict
-        hub_stats[hub] = {"Today":today, "DoD":dod, "Week Avg":wk_avg, "120 Avg":avg}
-
-    # Create df
-    hub_stats = pd.DataFrame.from_dict(hub_stats, orient="index")
-
-    # Create additional indicators
-    hub_stats["Change DoD"] = hub_stats["DoD"] / (hub_stats["Today"] - hub_stats["DoD"])
-    hub_stats["Change DoW"] = (hub_stats["Today"] - hub_stats["Week Avg"]) / hub_stats["Week Avg"]
-
-    # Filter bad values
-    hub_stats = hub_stats[hub_stats["Today"] != 0.0]
-    hub_stats = hub_stats[hub_stats["Change DoD"] != np.inf]
     
-    # Sort and print
-    print("Largest positive DoD percentage change:")
-    print(hub_stats.sort_values(by=["Change DoD"], ascending=False).head())
-    print("Largest positive DoW percentage change:")
-    print(hub_stats.sort_values(by=["Change DoW"], ascending=False).head())
-
-    print("Largest negative DoD percentage change:")
-    print(hub_stats.sort_values(by=["Change DoD"]).head())
-    print("Largest negative DoW percentage change:")
-    print(hub_stats.sort_values(by=["Change DoW"]).head())
